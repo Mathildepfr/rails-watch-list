272800e806567e50217ba46a9ae9e2b8b8b3768d
Mathilde Peiffer <peiffermathilde@gmail.com>
2021-08-27 12:46:54 +1000

    delete lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,6 +21,13 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: [:index, :new, :show, :create] do
+  resources :lists, only: [:index, :new, :show, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
